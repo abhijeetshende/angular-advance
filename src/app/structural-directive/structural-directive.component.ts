cc1b8651a712809ef5a112a76be4bb9c1228eaff
Abhijeet Shende <GS-3467@GSLAB.COM>
2023-01-18 17:51:19 +0530

    Structraldirective

--- src/app/structural-directive/structural-directive.component.ts
+++ src/app/structural-directive/structural-directive.component.ts
@@ -7,12 +7,18 @@ import { Subscription, timer } from 'rxjs';
   styleUrls: ['./structural-directive.component.scss'],
 })
 export class StructuralDirectiveComponent implements OnInit {
-  // intervalSubscription: any;
+  intervalSubscription: any;
 
   ngOnInit(): void {
-    setInterval(()=>{
+    this.intervalSubscription = setInterval(()=>{
+      console.log('updating structural directive');
+      
       this.isVisible = !this.isVisible;
     },1000)
   }
   isVisible: boolean = false;
+
+  ngOnDestroy(){
+    clearInterval(this.intervalSubscription)
+  }
 }
